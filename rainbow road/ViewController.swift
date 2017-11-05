1827611d0b4a6ab17748505062bb18b44e77b640
Elva Wang <elva316@gmail.com>
2017-11-05 12:37:35 -0800

    Initial Commit

---
+++ rainbow road/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainbow road
+//
+//  Created by elva wang on 11/5/17.
+//  Copyright © 2017 elva wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
